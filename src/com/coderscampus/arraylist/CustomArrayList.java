25bf39f4a338939aa4f747204e20d356227746cf
Ahexsel <130521411+Ahexsel@users.noreply.github.com>
2024-01-03 21:34:57 -0500

    Initial commit

---
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -0,0 +1,38 @@
+package com.coderscampus.arraylist;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Object[] items = new Object[10];
+	private int size = 0;
+	
+
+	@Override
+	public boolean add(T item) {
+		if (size == items.length) {
+			Object[] newData = new Object[items.length *2];
+										//used to double size of array 
+			System.arraycopy(items, 0, newData, 0, items.length);
+							//saved old array but adds any new elements 
+			items = newData;		
+		}
+		
+		items[size] = item;
+		size++;
+		return true;
+	
+	}
+
+	@Override
+	public int getSize() {
+		return 0;
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) items[index];
+        }
+}
---
+++ src/com/coderscampus/arraylist/DogApplication.java
@@ -0,0 +1,27 @@
+package com.coderscampus.arraylist;
+
+public class DogApplication {
+
+	public static void main(String[] args) {
+		CustomArrayList <String> dog = new CustomArrayList <>();
+		dog.add("Pug");
+		dog.add("Bulldog");
+		dog.add("Golden Retriever");
+		dog.add("Husky");
+		dog.add("Beagle");
+		dog.add("Dachshund");
+		dog.add("Border Collie");
+		dog.add("Poodle");
+		dog.add("Dobermann");
+		dog.add("Bully");
+		dog.add("Dalmatian");
+		dog.add("Shiba Inu");
+		dog.add("Chow");
+		
+		for (int i=0; i<13; i++) {
+		    System.out.println(dog.get(i));
+		}
+
+	}
+
+}
